7ffa966c00518c71b953a39f0fbfb8d20a72bbed
ekobara <emmanuelkobara@gmail.com>
2022-02-23 14:10:57 -0500

    MUI

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <label htmlFor="username"></label>
+                <input type="email" name="username" />
+                <label htmlFor="password"></label>
+                <input type="password" name="" />
+            </form>
+        </div>
+    );
+};
+
+export default Form;
